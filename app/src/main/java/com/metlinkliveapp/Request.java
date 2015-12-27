585e72f802bd9c3f0bff0b545442a4d01187a5fb
iddy <lemon.and.lime@hotmail.com>
2015-12-28 12:45:24 +1300

    changed Request to use Jsoup added getPage method to get URL added Jsoup library to project

--- app/src/main/java/com/metlinkliveapp/Request.java
+++ app/src/main/java/com/metlinkliveapp/Request.java
@@ -1,10 +1,29 @@
 package com.metlinkliveapp;
 
-import org.apache.http.HttpMessage;
+// import org.apache.http.HttpMessage;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.util.Log;
 
 /**
  * Created by iddy on 22/12/2015.
  */
 public class Request {
-
+    public static String getPage(Context context, String url) {
+        String text;
+        try {
+            Document doc = Jsoup.connect(url).get();
+            text = doc.text();
+        }
+        catch (IOException e) {
+            Log.d(context.getString(R.string.logname),e.getMessage());
+            return "Error";
+        }
+        Log.d("metlinkliveapp",text);
+        return text;
+    }
 }
